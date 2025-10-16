f2a41b5afc723b8264f28497f7df01e90c2cff1f
ThererGit <wtherer@hotmail.com>
2025-10-16 15:12:50 +0200

    V2.0 Display Rotary Angle Sensor Reading with LCD
    ajout du potentiomètre

--- Ex3_Contrôle_temp.py
+++ Ex3_Contrôle_temp.py
@@ -1,19 +1,26 @@
-## V 1. 0   Project 1: Display Hello, world! with LCD
-
-
+##V 2.0 Project 2: Display Rotary Angle Sensor Reading with LCD
 
 from lcd1602 import LCD1602
-from machine import I2C,Pin
+from machine import I2C, Pin, ADC
 from utime import sleep
 
-i2c=I2C(1,scl=Pin(7), sda=Pin(6), freq=400000)
-d=LCD1602(i2c,2,16)
 
-d.display() #allume l'écran
-sleep(1)
-d.clear()
-d.print('Bonjour')
+def main():
+    ROTARY_ANGLE_SENSOR = ADC(0)
+    i2c = I2C(1, scl=Pin(7), sda=Pin(6), freq=400000)
+    d = LCD1602(i2c, 2, 16)
 
-sleep(1)
-d.setCursor(0,1)
-d.print('Monde')
+    d.display()  # allume l'écran
+
+    while True:
+        sleep(1)
+        d.clear()  
+        d.setCursor(0,0)
+        d.print('Test Potentiometre')
+        d.setCursor(0, 1)
+        d.print(str(ROTARY_ANGLE_SENSOR.read_u16()))
+        sleep(0.2)
+
+
+###Exécution
+main()
